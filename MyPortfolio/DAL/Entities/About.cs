94355fdbfcd39cebbd9505ee27fc94920dd19f99
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-09 18:55:52 +0300

    portfolio entities added

---
+++ MyPortfolio/DAL/Entities/About.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public int Title { get; set; }
+        public string Subdescription { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ MyPortfolio/DAL/Entities/Testimonal.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Testimonal
+    {
+        public int TestimonalId { get; set; }
+        public string FullName { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
