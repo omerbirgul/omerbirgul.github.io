e5fd4c917365542546acfd7bdebecbe4ff41fcc9
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-09 21:28:44 +0300

    portfolio all viewComponents added

---
+++ MyPortfolio/ViewComponents/_ScriptComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _ScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
