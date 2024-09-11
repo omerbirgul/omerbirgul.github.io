d6a91f97a089b2088a7ebeaf68216ea42b561d3c
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-09-11 19:18:50 +0300

    portfolio admin dashboard list, add, delete experience methods added

---
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IActionResult ExperienceList()
+        {
+            var values = portfolioContext.Experiences.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            portfolioContext.Experiences.Add(experience);
+            portfolioContext.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = portfolioContext.Experiences.Find(id);
+            portfolioContext.Experiences.Remove(values);
+            portfolioContext.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+    }
+}
