8df30000d00f48512150bbc9149f46dbad591cf3
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-11-20 02:15:18 +0300

    Navbar jsx added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Pages/Home/HomeScreen";
+import Navbar from './Pages/Home/HomeScreen/Navbar';
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <div>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='*' element={<div>404 Not Found</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
